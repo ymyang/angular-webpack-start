0c692886fad1d7b0d5e4f353fd7e84156ca2baac
ymyang <ymyang@163.com>
2016-04-21 17:13:02 +0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     entry: {
         app: './app/js/main.js'
